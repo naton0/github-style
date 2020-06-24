76573a765c3ffed4fc4e5f2149405dea0bb4f62b
Anton <accounts@aj-creative.de>
2020-06-25 00:00:29 +0200

    upload

---
+++ style.js
@@ -0,0 +1,12 @@
+try {
+    let container = document.getElementById('js-repo-pjax-container');
+    document.getElementsByClassName('repohead')[0].style.boxShadow = 'inset 0 -1px 0 #e1e4e8';
+    container.childNodes[1].childNodes.forEach(function(item){
+        if(item.nodeType === 1) {
+            item.classList.add('container-xl')
+        }
+    });
+} catch(e) {
+    console.log(e);
+}
+
